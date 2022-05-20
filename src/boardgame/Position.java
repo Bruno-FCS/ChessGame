be783afaacce8277938ec2e9b1e90b3b4bbff106
Bruno Fernandes da Cunha Santos <fernandesbruno3232@hotmail.com>
2022-05-20 15:15:10 -0300

    Created Position class

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,5 +1,28 @@
 package boardgame;
 
 public class Position {
+	private Integer row;
+	private Integer column;
 
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public Integer getRow() {
+		return row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+
+	public void setValues(int row, int column) {
+
+	}
+
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
 }

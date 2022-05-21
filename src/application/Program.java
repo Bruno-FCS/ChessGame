ab2d8854c7bfb234d498ecc514dfb504eca53fd4
Bruno Fernandes da Cunha Santos <fernandesbruno3232@hotmail.com>
2022-05-21 16:12:41 -0300

    Printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,6 +22,11 @@ public class Program {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
+
+				boolean[][] possibleMoves = cm.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(cm.getPieces(), possibleMoves);
+
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
